256a95b6a9a24df144a713b5716b11c6d2a7ca6b
Gregory Moyer <moyerg@syphr.com>
2020-07-26 10:51:25 -0400

    Fix quoting in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@ set -eo pipefail
 shopt -s failglob
 
 if [ "${1}" = 'squeezeboxserver' ]; then
-    chown -R ${LMS_USER}:${LMS_GROUP} .
-    exec gosu ${LMS_USER} "$@"
+    chown -R "${LMS_USER}:${LMS_GROUP}" /usr/share/squeezeboxserver/
+    chown -R "${LMS_USER}:${LMS_GROUP}" .
+    exec gosu "${LMS_USER}" "$@"
 fi
 
 exec "$@"
