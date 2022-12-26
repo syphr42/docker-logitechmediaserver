c126c51d74898173f3973150544fd2109a9bae9c
Gregory Moyer <greg@commitpush.run>
2022-12-25 19:35:55 -0500

    Be verbose when changing file ownership
    This change also adds another directory to chown and is explicit about /var/lib/squeezeboxserver instead of assuming it is the working dir.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@ set -eo pipefail
 shopt -s failglob
 
 if [ "${1}" = 'squeezeboxserver' ]; then
-    chown -R "${LMS_USER}:${LMS_GROUP}" /usr/share/squeezeboxserver/
-    chown -R "${LMS_USER}:${LMS_GROUP}" .
+    echo "Updating file system ownership..."
+    chown --verbose --recursive "${LMS_USER}:${LMS_GROUP}" /usr/share/squeezeboxserver/
+    chown --verbose --recursive "${LMS_USER}:${LMS_GROUP}" /var/lib/squeezeboxserver
+    chown --verbose --recursive "${LMS_USER}:${LMS_GROUP}" /var/log/squeezeboxserver
     exec gosu "${LMS_USER}" "$@"
 fi
 
