5940f8fa1317f3d9df042e2a85c36d457ef663f1
Gregory Moyer <greg@commitpush.run>
2022-12-25 20:46:36 -0500

    Disable verbose chown and add logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@ shopt -s failglob
 
 if [ "${1}" = 'squeezeboxserver' ]; then
     echo "Updating file system ownership..."
-    chown --verbose --recursive "${LMS_USER}:${LMS_GROUP}" /usr/share/squeezeboxserver/
-    chown --verbose --recursive "${LMS_USER}:${LMS_GROUP}" /var/lib/squeezeboxserver
-    chown --verbose --recursive "${LMS_USER}:${LMS_GROUP}" /var/log/squeezeboxserver
+    chown --recursive "${LMS_USER}:${LMS_GROUP}" /usr/share/squeezeboxserver/
+    chown --recursive "${LMS_USER}:${LMS_GROUP}" /var/lib/squeezeboxserver
+    chown --recursive "${LMS_USER}:${LMS_GROUP}" /var/log/squeezeboxserver
+
+    echo "Changing user to '${LMS_USER}' and starting LMS..."
     exec gosu "${LMS_USER}" "$@"
 fi
 
